ca56a3ba35e6c7c6c167461c37f553b5faa71e00
Rounak Suthar <rounaksolanki37@gmail.com>
2025-09-22 09:09:08 +0530

    Added Change Password Functionality

--- src/app/forgetpassword/forgetpassword.component.ts
+++ src/app/forgetpassword/forgetpassword.component.ts
@@ -1,27 +1,84 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatMenuContent } from "@angular/material/menu";
 
 @Component({
   selector: 'app-forgetpassword',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './forgetpassword.component.html',
   styleUrl: './forgetpassword.component.scss'
 })
 export class ForgetpasswordComponent {
-  submitted=false;
-  registrationform:FormGroup
-  constructor(){
-    this.registrationform=new FormGroup({});
 
+  username_email_exists=['ABC','XYZ@GMAIL.COM', 'PQR'];
+  otp='1234';
+  submitted=false;
+  passwordsubmit=false;
+  resetform:FormGroup;
+  resetpassword:FormGroup;
+  isenablereset:boolean=false;
+  isValidEmail:boolean = true;
+  ispasswordmatch:boolean=false;
+  validateotp:boolean=false;
+
+  constructor(private fb:FormBuilder){
+    this.resetform = this.fb.group({
+      Email:['',[Validators.required,Validators.pattern('^[A-za-z0-9._%+-@]+$')]]
+    }),
+    this.resetpassword = this.fb.group({
+      password:['',[Validators.required,Validators.minLength(5),Validators.maxLength(10)]],
+      repassword:['',[Validators.required,Validators.minLength(5),Validators.maxLength(10)]]
+    })
+  }
+  get f(){
+    return this.resetform.controls;
+  }
+
+  get f1(){
+    return this.resetpassword.controls;
   }
   onSubmit() {
     this.submitted=true;
-    if(this.registrationform.invalid)
+    if(this.resetform.invalid || !this.isValidEmail)
     {
-      this.registrationform.markAllAsTouched();
+      this.isenablereset=false;
+      this.resetform.markAllAsTouched();
       return;
     }
-    console.log(this.registrationform);
+    else
+      if(this.resetform.valid){
+       this.isenablereset=true;
+    }
+    console.log(this.resetform);
+  }
+
+  onSubmitted() {
+    this.passwordsubmit=true;
+    if(this.resetpassword.invalid || this.ispasswordmatch)
+    {
+      this.resetpassword.markAllAsTouched();
+      return;
+    }
+    console.log(this.resetpassword);
+  }
+
+  validateEmail() {
+    const email:string = String(this.resetform.value.Email);
+    console.log(email);
+    this.isValidEmail = this.username_email_exists.indexOf(email.toUpperCase()) > -1;
+  }
+
+  ValidatePasswordMatch(){
+    const pass1:string = String(this.resetpassword.value.repassword);
+    const pass2:string = String(this.resetpassword.value.password);
+    this.ispasswordmatch = pass1===pass2;
+    console.log(pass1+""+ pass2+ " "+this.ispasswordmatch);
+  }
+
+  validateotpMatch(){
+
+    
   }
 }
